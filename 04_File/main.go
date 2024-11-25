441eb7ce9de1fa83691d84a7f87e23987f5ff1e6
LuckyQu <super.lucky.qu@gmail.com>
2024-11-25 18:05:42 +0800

    更新Gin框架学习内容

---
+++ 04_File/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+func main() {
+	var err error
+	g := gin.Default()
+	g.POST("/FileTest", func(c *gin.Context) {
+		file, _ := c.FormFile("file")
+		log.Println(file.Filename)
+		err = c.SaveUploadedFile(file, "./"+file.Filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(200, gin.H{
+			"msg": file.Filename,
+		})
+	})
+	err = g.Run("localhost:8080")
+	if err != nil {
+		panic(err)
+	}
+}
