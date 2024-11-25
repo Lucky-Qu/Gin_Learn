225d4ffc4482eb2d5ac2fb4092c48e70e73a6248
LuckyQu <super.lucky.qu@gmail.com>
2024-11-25 20:40:22 +0800

    更新Gin框架学习内容

--- 06_Log/main.go
+++ 06_Log/main.go
@@ -2,22 +2,47 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 	"io"
 	"os"
+	"time"
 )
 
+func initLogrus() {
+	path := "./06_Log/LogTest.log"
+	logFile, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.WithError(err).Fatal("打开日志文件失败")
+	}
+
+	log.SetOutput(io.MultiWriter(logFile, os.Stdout))
+	log.SetFormatter(&log.TextFormatter{
+		ForceColors:     true,
+		FullTimestamp:   true,
+		TimestampFormat: "2006-01-02 15:04:05",
+	})
+	log.SetLevel(log.InfoLevel)
+}
+
+func middleLog() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		StartTime := time.Now()
+		c.Next()
+		EndTime := time.Now()
+		initLogrus()
+		log.WithFields(log.Fields{
+			"StartTime": StartTime,
+			"EndTime":   EndTime,
+		}).Info("处理请求")
+	}
+}
 func main() {
 	var err error
+
 	g := gin.Default()
-	//禁用颜色
-	gin.DisableConsoleColor()
-	//记录到文件
-	f, err := os.Create("gin.log")
-	if err != nil {
-		panic(err)
-	}
-	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
-	g.POST("/LogTest", func(c *gin.Context) {
+	v1 := g.Group("/v1")
+	v1.Use(middleLog())
+	v1.POST("/LogTest", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"msg": "执行成功",
 		})
